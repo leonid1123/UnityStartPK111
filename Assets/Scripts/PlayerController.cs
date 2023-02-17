03f2b8cc2544cab40df87b9550d3563e129afb41
leonid1123 <47742726+leonid1123@users.noreply.github.com>
2023-02-17 17:57:44 +0300

    movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,9 +6,11 @@ public class PlayerController : MonoBehaviour
     public Rigidbody2D rb2d;
     float spd=10f;
     bool canJump = true;
+    public Animator anim;
     void Start()
     {
-        rb2d = gameObject.GetComponent<Rigidbody2D>();       
+        rb2d = gameObject.GetComponent<Rigidbody2D>(); 
+        anim = gameObject.GetComponent<Animator>();      
     }
 
     // Update is called once per frame
@@ -18,9 +20,12 @@ public class PlayerController : MonoBehaviour
         rb2d.velocity = new Vector2(Input.GetAxisRaw("Horizontal")*spd,rb2d.velocity.y);
         //Debug.Log("Скорость:" + Input.GetAxisRaw("Horizontal"));
         if(Input.GetButtonUp("Jump") & canJump) {
-            rb2d.AddRelativeForce(Vector2.up*5,ForceMode2D.Impulse);
+            rb2d.AddRelativeForce(Vector2.up*8,ForceMode2D.Impulse);
             canJump=false;
         }
+
+        anim.SetFloat("mov",rb2d.velocity.sqrMagnitude);
+        
     }
     private void OnTriggerEnter2D(Collider2D other) {
         Debug.Log("Ой!");
